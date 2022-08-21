c58b1b15815a47eba8c5d0a6c4e81e8d7dc3a38d
aggreyah <aggreyah@gmail.com>
2022-08-21 21:59:18 +0300

    added navigation view menu items behavior.

--- NoteKeeper/app/src/main/java/com/aggreyah/notekeeper/ItemsActivity.kt
+++ NoteKeeper/app/src/main/java/com/aggreyah/notekeeper/ItemsActivity.kt
@@ -3,22 +3,31 @@ package com.aggreyah.notekeeper
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aggreyah.notekeeper.databinding.ActivityItemsBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class ItemsActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityItemsBinding
+
+    private val noteLayoutManager by lazy{LinearLayoutManager(this)}
+
+    private val noteRecyclerAdapter by lazy { NoteRecyclerAdapter(this, DataManager.notes) }
+
+    private val courseLayoutManager by lazy { GridLayoutManager(this, 2) }
+
+    private val courseRecyclerAdapter by lazy { CourseRecyclerAdapter(this, DataManager.courses.values.toList()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +41,7 @@ class ItemsActivity : AppCompatActivity() {
             startActivity(Intent(this, NoteActivity::class.java))
         }
 
-        binding.appBarItems.includedLayoutContent.listItems.layoutManager = LinearLayoutManager(this)
-        binding.appBarItems.includedLayoutContent.listItems.adapter = NoteRecyclerAdapter(this, DataManager.notes)
+        displayNotes()
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
@@ -42,11 +50,30 @@ class ItemsActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_notes, R.id.nav_courses
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_notes -> displayNotes()
+                R.id.nav_courses -> displayCourses()
+            }
+            return@setNavigationItemSelectedListener true
+        }
+    }
+
+    private fun displayNotes() {
+        binding.appBarItems.includedLayoutContent.listItems.layoutManager = noteLayoutManager
+        binding.appBarItems.includedLayoutContent.listItems.adapter = noteRecyclerAdapter
+        binding.navView.menu.findItem(R.id.nav_notes).isChecked = true
+    }
+
+    private fun displayCourses() {
+        binding.appBarItems.includedLayoutContent.listItems.layoutManager = courseLayoutManager
+        binding.appBarItems.includedLayoutContent.listItems.adapter = courseRecyclerAdapter
+        binding.navView.menu.findItem(R.id.nav_courses).isChecked = true
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -64,4 +91,6 @@ class ItemsActivity : AppCompatActivity() {
         super.onResume()
         binding.appBarItems.includedLayoutContent.listItems.adapter?.notifyDataSetChanged()
     }
+
+
 }
