76d5e07392a279a78e5e30e17d5a17065e979bb2
aggreyah <aggreyah@gmail.com>
2022-09-25 13:50:42 +0300

    navigation view is now working.

--- GloboTour/app/src/main/java/com/aggreyah/globotour/MainActivity.kt
+++ GloboTour/app/src/main/java/com/aggreyah/globotour/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.aggreyah.globotour
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.aggreyah.globotour.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar        : MaterialToolbar
+    private lateinit var navController  : NavController
+    private lateinit var binding        : ActivityMainBinding
+    private lateinit var navigationView : NavigationView
+    private lateinit var drawerLayout   : DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        toolbar = binding.activityMainToolbar
+        navigationView = binding.navView
+        drawerLayout = binding.drawerLayout
+        val navHost = supportFragmentManager.findFragmentById(binding.navHostFrag.id) as NavHostFragment
+        navController = navHost.navController
+
+        // Define AppBarConfiguration: Connect DrawerLayout with Navigation Component
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        // Connect Toolbar with NavController
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        // Connect NavigationView with NavController
+        navigationView.setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isOpen)
+            drawerLayout.close()
+        else
+            super.onBackPressed()
     }
 }
---
+++ GloboTour/app/src/main/java/com/aggreyah/globotour/city/CityListFragment.kt
@@ -0,0 +1,18 @@
+package com.aggreyah.globotour.city
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aggreyah.globotour.R
+
+class CityListFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_city_list, container, false)
+    }
+}
