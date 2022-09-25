7cbf137388ee6b17ac8b83fdd9a585ba61e686fa
aggreyah <aggreyah@gmail.com>
2022-09-25 15:15:34 +0300

    added RecyclerView to the citylist fragment.

--- GloboTour/app/src/main/java/com/aggreyah/globotour/city/CityListFragment.kt
+++ GloboTour/app/src/main/java/com/aggreyah/globotour/city/CityListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.aggreyah.globotour.R
 
 class CityListFragment: Fragment() {
@@ -13,6 +15,20 @@ class CityListFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_city_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_city_list, container, false)
+        setUpRecyclerView(view)
+        return view
+    }
+
+    private fun setUpRecyclerView(view: View?) {
+        val context = requireContext()
+        val cityAdapter = CityAdapter(context, VacationSpots.cityList!!)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.city_recycler_view)
+        recyclerView?.adapter = cityAdapter
+        recyclerView?.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = RecyclerView.VERTICAL
+        recyclerView?.layoutManager = layoutManager
     }
 }
