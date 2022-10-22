46b8a67680ce16865f7377ad3d61b61ebd25d24c
aggreyah <aggreyah@gmail.com>
2022-10-22 17:14:05 +0300

    added drag to rearrange plus swipe to delete feature to the favorite city list. swipe to delete featrure is however not working. we shall discover latrer in my coding journey hopefully.

--- GloboTour/app/src/main/java/com/aggreyah/globotour/favorite/FavoriteFragment.kt
+++ GloboTour/app/src/main/java/com/aggreyah/globotour/favorite/FavoriteFragment.kt
@@ -6,13 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aggreyah.globotour.R
+import com.aggreyah.globotour.city.City
 import com.aggreyah.globotour.city.VacationSpots
+import com.google.android.material.snackbar.Snackbar
+import java.util.Collections
 
 
-class FavoriteFragment: Fragment() {
+class FavoriteFragment : Fragment() {
+    private lateinit var favoriteCityList: ArrayList<City>
+    private lateinit var favoriteAdapter: FavoriteAdapter
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,16 +31,70 @@ class FavoriteFragment: Fragment() {
         return view
     }
 
-    private fun setUpRecyclerView(view: View?) {
+    private fun setUpRecyclerView(view: View) {
         val context = requireContext()
-        val favoriteAdapter = FavoriteAdapter(context, VacationSpots.favoriteCityList)
-        val recyclerView = view?.findViewById<RecyclerView>(R.id.favorite_recycler_view)
-        recyclerView?.adapter = favoriteAdapter
-        recyclerView?.setHasFixedSize(true)
+        favoriteCityList = VacationSpots.favoriteCityList as ArrayList<City>
+        favoriteAdapter = FavoriteAdapter(context, favoriteCityList)
+
+        recyclerView = view.findViewById(R.id.favorite_recycler_view)
+        recyclerView.adapter = favoriteAdapter
+        recyclerView.setHasFixedSize(true)
 
         val layoutManager = LinearLayoutManager(context)
         layoutManager.orientation = RecyclerView.VERTICAL
-        recyclerView?.layoutManager = layoutManager
+        recyclerView.layoutManager = layoutManager
 
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
+    private val itemTouchHelper = ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0){
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            targetViewHolder: RecyclerView.ViewHolder
+        ): Boolean {
+            val fromPosition = viewHolder.adapterPosition
+            val toPosition = targetViewHolder.adapterPosition
+
+            Collections.swap(favoriteCityList, fromPosition, toPosition)
+
+            recyclerView.adapter?.notifyItemMoved(fromPosition, toPosition)
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            val deletedCity: City = favoriteCityList[position]
+            
+            deleteItem(position)
+            updateCityList(deletedCity, false)
+
+            Snackbar.make(recyclerView, "Deleted", Snackbar.LENGTH_LONG)
+                .setAction("UNDO"){
+                    undoDelete(position, deletedCity)
+                    updateCityList(deletedCity, true)
+                }
+                .show()
+        }
+
+    })
+
+    private fun undoDelete(position: Int, deletedCity: City) {
+        favoriteCityList.add(position, deletedCity)
+        favoriteAdapter.notifyItemInserted(position)
+        favoriteAdapter.notifyItemRangeChanged(position, favoriteCityList.size)
+    }
+
+    private fun updateCityList(deletedCity: City, isFavorite: Boolean) {
+        val cityList = VacationSpots.cityList!!
+        val position = cityList.indexOf(deletedCity)
+        cityList[position].isFavorite = isFavorite
+    }
+
+    private fun deleteItem(position: Int) {
+        favoriteCityList.removeAt(position)
+        favoriteAdapter.notifyItemRemoved(position)
+        favoriteAdapter.notifyItemRangeChanged(position, favoriteCityList.size)
     }
 }
