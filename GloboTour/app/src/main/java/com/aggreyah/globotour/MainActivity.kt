35ef383cf91533013558b2f2c8eb9aa9f72883a4
aggreyah <aggreyah@gmail.com>
2022-10-22 14:12:47 +0300

    implemented the bottom navigation bar.

--- GloboTour/app/src/main/java/com/aggreyah/globotour/MainActivity.kt
+++ GloboTour/app/src/main/java/com/aggreyah/globotour/MainActivity.kt
@@ -9,39 +9,32 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.aggreyah.globotour.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var toolbar        : MaterialToolbar
     private lateinit var navController  : NavController
     private lateinit var binding        : ActivityMainBinding
-    private lateinit var navigationView : NavigationView
-    private lateinit var drawerLayout   : DrawerLayout
+    private lateinit var bottomNavView  : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         toolbar = binding.activityMainToolbar
-        navigationView = binding.navView
-        drawerLayout = binding.drawerLayout
         val navHost = supportFragmentManager.findFragmentById(binding.navHostFrag.id) as NavHostFragment
         navController = navHost.navController
+        bottomNavView = binding.bottomNavView
 
-        // Define AppBarConfiguration: Connect DrawerLayout with Navigation Component
-        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        // Define AppBarConfiguration
+        val topLevelDestinations = setOf(R.id.fragmentCityList, R.id.fragmentFavoriteList)
+        val appBarConfiguration = AppBarConfiguration(topLevelDestinations)
 
         // Connect Toolbar with NavController
         toolbar.setupWithNavController(navController, appBarConfiguration)
 
-        // Connect NavigationView with NavController
-        navigationView.setupWithNavController(navController)
-    }
-
-    override fun onBackPressed() {
-        if (drawerLayout.isOpen)
-            drawerLayout.close()
-        else
-            super.onBackPressed()
+        // Connect BottomNavigationView with NavController
+        bottomNavView.setupWithNavController(navController)
     }
 }
